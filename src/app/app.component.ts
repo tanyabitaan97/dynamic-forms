3c5adaa817adca7a943b090bc477c46b8bc46ab3
unknown <tanyabitaan.97@gmail.com>
2024-08-07 13:54:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dynamicforms';
+  name = 'Angular';
+  formGroup = this.fb.group({
+    arr: this.fb.array([this.createItem()])
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+
+
+    // this.formGroup.valueChanges.subscribe(rs => {});
+    // this.f.arr.controls[0].name.valueChanges.subscribe(rs => {
+    //   console.log(rs);
+    //   this.f.title.setValidators([Validators.required]);
+    //   this.f.title.clearValidators();
+    //   this.f.title.updateValueAndValidity();
+    // })
+  }
+
+  get f() {
+    return this.formGroup.get('arr') as FormArray;
+  }
+
+  createItem() {
+    return this.fb.group({
+      name: ['', Validators.required]
+    });
+  }
+
+  addItem() {
+
+    (this.formGroup.get('arr') as FormArray).push(this.createItem());
+  }
+
+  removeItem(idx: number): void {
+    (this.formGroup?.controls['arr'] as FormArray).removeAt(idx);
+  }
+
+  onSubmit() {
+    this.formGroup?.markAllAsTouched();
+    console.log(this.formGroup?.value);
+  }
 }
